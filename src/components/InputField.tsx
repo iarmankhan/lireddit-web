7db70d0bb9cff9a32de2a68aae2ee8c11dfc5bb5
iarmankhan <armankhan9244@gmail.com>
2020-10-10 22:33:36 +0530

    Added create post form, layout file, isAuth hook

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -3,6 +3,7 @@ import {
     FormLabel,
     Input,
     FormErrorMessage,
+    Textarea,
 } from "@chakra-ui/core";
 import { useField } from "formik";
 import React, { InputHTMLAttributes } from "react";
@@ -10,19 +11,23 @@ import React, { InputHTMLAttributes } from "react";
 type InputFieldProps = InputHTMLAttributes<HTMLInputElement> & {
     label: string;
     name: string;
+    textarea?: boolean;
 };
 
 export const InputField: React.FC<InputFieldProps> = ({
     label,
     size: _,
+    textarea,
     ...props
 }) => {
     const [field, { error }] = useField(props);
 
+    let C = !textarea ? Input : Textarea;
+
     return (
         <FormControl isInvalid={!!error}>
             <FormLabel htmlFor={field.name}>{label}</FormLabel>
-            <Input
+            <C
                 {...field}
                 {...props}
                 id={field.name}
